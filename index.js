11ebc293cebe024a3d3269772ce118b7f88c1e85
garfias06 <garfias06_23@outlook.com>
2023-04-30 16:18:59 -0500

    js-modified

---
+++ index.js
@@ -0,0 +1,57 @@
+// All my questions
+// !Import node packages and js files
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const logoMaker = require('./lib/shapes')
+
+// svg size:300x200 px
+
+// prompt:
+// !up to three characters per shape (letters on logo)
+// !text color: color keyword or hexadecimal number
+// !LIST of shapes: circle, triangle, square
+// !shape color(shape background): keyword or hexadecimal number
+
+const shapeSpecs = [
+    {
+        type: 'list',
+        message: 'Select shape:',
+        name: 'shape',
+        choices: ['circle', 'triangle', 'square'],
+    },
+    {
+        type: 'input',
+        message: 'Enter a background color:',
+        name: 'shapeColor',
+      },
+      {
+        type: 'input',
+        message: 'Text, enter up to three characters:',
+        name: 'text',
+      },
+      {
+        type: 'input',
+        message: 'Enter text color:',
+        name: 'textColor',
+      },
+];
+
+// when done with questions:
+// create SVG FILE and name it: logo.svg
+// console.log:Generated logo.svg
+
+function createNewFile(){
+    inquirer.prompt(shapeSpecs)
+    .then((data) => {
+      console.log(data)
+      writeToFile('logo.svg', logoMaker(data))
+    })
+}
+function writeToFile(logo, data){
+    fs.writeFile(logo, data, (err) => err ? console.error(err) : console.log('Generated logo.svg'))
+}
+createNewFile();
+
+// importing and exporting modules
